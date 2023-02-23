9a5e7f1f02cf78dbe5bdea16e8b9deaf3e5bdcf6
momo-0129 <tonyfang8733@gmail.com>
2023-02-23 21:47:54 +0900

    editpage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,34 @@ class ArticlesController < ApplicationController
     @articles = Article.all 
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article created successfully"
+      redirect_to @article
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Updated article"
+      redirect_to @article
+    else
+      render :new, status: :bad_request
+    end
+
+  end
+
 end
