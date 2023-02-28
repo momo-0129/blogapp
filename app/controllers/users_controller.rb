f1a957a665c28c62b81bb90f9b7553baa1bdfc89
momo-0129 <tonyfang8733@gmail.com>
2023-02-28 16:56:45 +0900

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the MOMO BLOG #{@user.username}, you have successfully signed up."
+      redirect_to articles_path
+    else   
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
